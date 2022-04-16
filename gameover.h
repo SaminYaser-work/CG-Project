386c2f8a5e95eb89d8267444a2a48540827cf70d
Shanjidul Islam Sadhin <66504343+sadhiin@users.noreply.github.com>
2022-04-16 23:26:23 +0600

    Created gameover. h
    Game over screen added

---
+++ gameover.h
@@ -0,0 +1,15 @@
+#ifndef GAMEOVER_H_INCLUDED
+#define GAMEOVER_H_INCLUDED
+
+#include <string>
+#include "texture.h"
+#include "printText.h"
+
+void gameOver(){
+    displayTexture("E:\\CG-Project\\textures\\gmo2.jpg", 600, 1000, 1400, 1000, 1400, 1750, 600, 1750);
+
+    // Add text to {to play agian}
+    std::string contText="Press <space> to play again";
+    printText(contText,690.0,1030.0, 0.3, 2);
+}
+#endif // GAMEOVER_H_INCLUDED
